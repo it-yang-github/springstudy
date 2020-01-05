98548fd764d1101a75bf30b9e7626d19074a4a8f
yqw <qq.com>
2020-01-05 15:00:01 +0800

    集成通用mapper

---
+++ microservicecloud-provider-dept/src/main/java/cn/qw/microservicecloud/dao/DepyDao.java
@@ -0,0 +1,9 @@
+package cn.qw.microservicecloud.dao;
+
+import cn.qw.springcloud.enty.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DepyDao extends IBaseDao<Dept> {
+
+}
